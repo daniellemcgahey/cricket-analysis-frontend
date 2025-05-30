231659c07e5cfa304f18f364e21a1cfb561dbeab
daniellemcgahey <daniellemcgahey1994@hotmail.com>
2025-05-30 20:59:14 -0300

    Display Changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://cricket-analysis-api.onrender.com", // fallback in case env variable is missing
 });
 
 export default api;
